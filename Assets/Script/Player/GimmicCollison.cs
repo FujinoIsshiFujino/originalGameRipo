25bc97eec262fc97e7736430fff48fa3131cd499
FujinoIsshiFujino <issi1208@icloud.com>
2024-01-04 21:09:43 +0900

    GimmicCollison　リファクタ

--- Assets/Script/Player/GimmicCollison.cs
+++ Assets/Script/Player/GimmicCollison.cs
@@ -8,43 +8,31 @@ public class GimmicCollison : MonoBehaviour
     Vector3 playerForward;
     Vector3 objForward;
     public float angleInDegrees;
-    
 
 
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerStay(Collider other)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-      
-    }
 
-   private void OnTriggerStay(Collider other) {
-    
-        
 
-    //　とりあえず正面のみOK
-    {
-        if(other.gameObject.tag == "Treasure")
+        //　とりあえず正面のみOK
         {
-
-            objForward = other.gameObject.transform.forward.normalized;
-            playerForward = Player.transform.forward.normalized;
-
-            float dotProduct = Vector3.Dot(objForward, playerForward);
-            float angleInRadians = Mathf.Acos(dotProduct);
-            angleInDegrees = angleInRadians * Mathf.Rad2Deg;
-
-            if(angleInDegrees >= 120 && angleInDegrees<=180)
+            if (other.gameObject.tag == "Treasure")
             {
-                Debug.Log("Treasure");
-            }
 
+                objForward = other.gameObject.transform.forward.normalized;
+                playerForward = Player.transform.forward.normalized;
+
+                float dotProduct = Vector3.Dot(objForward, playerForward);
+                float angleInRadians = Mathf.Acos(dotProduct);
+                angleInDegrees = angleInRadians * Mathf.Rad2Deg;
+
+                if (angleInDegrees >= 120 && angleInDegrees <= 180)
+                {
+                    Debug.Log("Treasure");
+                }
+
+            }
         }
     }
-   }
 }
