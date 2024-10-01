8572735ddbac6d7a4559cb54744184e9bb336f4b
FujinoIsshiFujino <issi1208@icloud.com>
2024-10-02 07:53:51 +0900

    ２段攻撃の実装

--- Assets/Script/Player/State/StateAttacking.cs
+++ Assets/Script/Player/State/StateAttacking.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,29 @@ public partial class PlayerControl
     [SerializeField] private Collider attackCollider;
     [SerializeField] private float attackCoolDown = 0.5f;
     bool isRedyAttack = true;
+    public bool attackFinished = false; // ２段攻撃に移行するかのフラグ
     public class StateAttacking : PlayerStateBase
     {
         public override void OnEnter(PlayerControl owner, PlayerStateBase preState)
         {
             owner.isRedyAttack = false;
-
         }
+
         public override void OnUpdate(PlayerControl owner)
         {
+            // 現在再生中のアニメーション情報を取得
+            AnimatorStateInfo stateInfo = owner._animator.GetCurrentAnimatorStateInfo(0);
 
-
-
+            // アニメーションが"Attack"の間だけ時間を計測
+            if (stateInfo.IsName("Attack"))
+            {
+                // 攻撃ボタンが再度押されていれば、2段攻撃に遷移
+                if (Input.GetButtonDown("Attack"))
+                {
+                    owner.attackFinished = true;
+                    return;
+                }
+            }
         }
 
         public override void OnExit(PlayerControl owner, PlayerStateBase nextState)
@@ -26,7 +38,6 @@ public partial class PlayerControl
 
         }
     }
-
 
     //攻撃コリジョンにあたったらインスペクター上から呼ばれる
     public void HitAttack(Collider collider)
@@ -37,22 +48,37 @@ public partial class PlayerControl
         targetMob.Damage(1);
     }
 
-
-
-
-    //モーションの始まりと終わりに呼ばれる
-    //終わりに呼ばれる方はステイト遷移もかねてる
+    //モーションの途中（攻撃コリジョンが発生してほしいタイミング）で呼ばれる
     public void AttackStart()
     {
-
         attackCollider.enabled = true;
     }
 
+    //モーションの途中（攻撃コリジョンが消えてほしいタイミング）で呼ばれる
     public void AttackFinished()
     {
         attackCollider.enabled = false;
+
+        if (attackFinished)
+        {
+            _animator.SetTrigger("Attack2");
+            ChangeState(stateAttacking);
+            attackFinished = false;
+            return;
+        }
+
+        attackFinished = false;
         StartCoroutine(CoolDownCoroutine());
     }
+
+    public void SecondAttackFinished()
+    {
+        attackCollider.enabled = false;
+        attackFinished = false;
+
+        StartCoroutine(CoolDownCoroutine());
+    }
+
     private IEnumerator CoolDownCoroutine()
     {
         yield return new WaitForSeconds(attackCoolDown);
